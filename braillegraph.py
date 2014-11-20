6225f192caeb8a46d87c887838c103a5352f95ae
Chris Bouchard <cbouchard@leadingageny.org>
2014-11-20 15:15:08 -0500

    Added gitignore and cleaned up some code.

--- braillegraph.py
+++ braillegraph.py
@@ -2,6 +2,7 @@
 
 import itertools
 
+
 # The Unicode codepoint for an empty braille blocks (no raised dots)
 _BRAILLE_EMPTY_BLOCK = 0x2800
 
@@ -18,7 +19,7 @@ _BRAILLE_HALF_ROW = [0x01, 0x02, 0x04, 0x40]
 _BRAILLE_FULL_ROW = [0x09, 0x12, 0x24, 0xC0]
 
 
-def chunk(iterable, size):
+def _chunk(iterable, size):
     '''Split an iterable into chunks of a fixed size.'''
     yield from (
         (item for index, item in group)
@@ -33,14 +34,19 @@ def braillegraph(bars):
     '''Doc string'''
     lines = []
 
-    # Each line can accomodate our bars, one for each row in the braille
-    # block.
-    for bar_group in chunk(bars, 4):
+    # Break the bars into groups of four, one for each row in the braille
+    # blocks.
+    for bar_group in _chunk(bars, 4):
         line = []
 
         for braille_row, bar_value in enumerate(bar_group):
-            # The number of braille blocks needed to draw this bar
-            blocks_needed = (bar_value // 2) + (bar_value % 2)
+            # The number of full braille blocks needed to draw this bar. Each
+            # block is two dots wide.
+            full_blocks_needed = bar_value // 2
+
+            # The number of braille blocks needed to draw this bar. The second
+            # term accounts for a possible half row.
+            blocks_needed = full_blocks_needed + (bar_value % 2)
 
             # The number of braille blocks we'll need to append to the current
             # line to accomodate this bar
@@ -51,19 +57,18 @@ def braillegraph(bars):
                 line.extend([_BRAILLE_EMPTY_BLOCK] * extra_blocks_needed)
 
             # Fill in the majority of the bar with full braille rows (two dots).
-            for block_index in range(bar_value // 2):
+            for block_index in range(full_blocks_needed):
                 line[block_index] += _BRAILLE_FULL_ROW[braille_row]
 
             # If the bar's value is odd, we'll need to add a single dot at the
             # end.
             if bar_value % 2:
-                block_index = (bar_value // 2)
-                line[block_index] += _BRAILLE_HALF_ROW[braille_row]
+                line[full_blocks_needed] += _BRAILLE_HALF_ROW[braille_row]
 
         # Wrap up this line by converting all the code points to characters
         # and concatenating them.
-        lines.append(line)
+        lines.append(''.join(chr(code) for code in line))
 
-    # Join all the lines
-    return '\n'.join(''.join(chr(code) for code in line) for line in lines)
+    # Join all the lines to make the final graph
+    return '\n'.join(lines)
 
