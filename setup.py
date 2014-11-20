f3a8aa7e00de8a4d17e7325786fd7d7b3f89b9f7
Chris Bouchard <cbouchard@leadingageny.org>
2014-11-20 16:19:39 -0500

    Make the setup.py more specific.

--- setup.py
+++ setup.py
@@ -8,7 +8,16 @@ setup(name='braillegraph',
       url='http://github.com/chrisbouchard/braillegraph',
       author='Chris Bouchard',
       author_email='chris@upliftinglemma.net',
-      license='BSD',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: End Users/Desktop',
+          'License :: OSI Approved :: BSD License',
+          'Natural Language :: English',
+          'Operating System :: POSIX',
+          'Programming Language :: Python :: 3 :: Only',
+          'Topic :: Utilities'
+      ],
       packages=['braillegraph'],
       zip_safe=False)
 
