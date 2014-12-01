86adf4b2ff846ae06fd7613c1c0375c4d7798231
Chris Bouchard <chris@upliftinglemma.net>
2014-12-01 14:44:58 -0500

    Add more tests.

--- braillegraph/tests/test_braillegraph.py
+++ braillegraph/tests/test_braillegraph.py
@@ -4,10 +4,11 @@ import unittest
 
 from .. import braillegraph
 
-# Don't blame this class for the sins of its father.
-# pylint: disable=too-many-public-methods
-class HorizontalTestCase(unittest.TestCase):
+class HorizontalGraphTestCase(unittest.TestCase):
     '''Test the braillegraph.horizontal_graph function.'''
+
+    # Don't blame this class for the sins of its father.
+    # pylint: disable=too-many-public-methods
 
     def test_one_line_range(self):
         '''Test a range of four numbers that fits one one line.'''
@@ -21,5 +22,40 @@ class HorizontalTestCase(unittest.TestCase):
         self.assertEqual(
             braillegraph.horizontal_graph(range(1, 7)),
             '⠀⠀⣠\n⣠⣾⣿'
+        )
+
+    def test_multi_args(self):
+        '''Test passing multiple arguments.'''
+        self.assertEqual(
+            braillegraph.horizontal_graph(3, 1, 4, 1, 5, 9, 2, 6),
+            '⠀⠀⢀\n⠀⠀⣸⢠\n⣆⣇⣿⣼'
+        )
+
+
+class VerticalGraphTestCase(unittest.TestCase):
+    '''Test the braillegraph.vertical_graph function.'''
+
+    # Don't blame this class for the sins of its father.
+    # pylint: disable=too-many-public-methods
+
+    def test_one_line_range(self):
+        '''Test a range of four numbers that fits one one line.'''
+        self.assertEqual(
+            braillegraph.vertical_graph(range(1, 5)),
+            '⣷⣄'
+        )
+
+    def test_two_line_range(self):
+        '''Test a range of four numbers that requires two lines.'''
+        self.assertEqual(
+            braillegraph.vertical_graph(range(1, 7)),
+            '⣷⣄\n⠛⠛⠓'
+        )
+
+    def test_multi_args(self):
+        '''Test passing multiple arguments.'''
+        self.assertEqual(
+            braillegraph.vertical_graph(3, 1, 4, 1, 5, 9, 2, 6),
+            '⡯⠥\n⣿⣛⣓⠒⠂'
         )
 
